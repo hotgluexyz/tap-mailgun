033cb4db46738a4277fda796b72d51d306673ffb
Josué M. Sehnem <josue@sehnem.com>
2022-01-03 10:39:46 -0500

    added members stream

--- src/tap_mailgun/const.py
+++ src/tap_mailgun/const.py
@@ -3,11 +3,14 @@ DEPENDENCIES = {
     # Parent stream: [Dependent streams]
     'events': ['bounces', 'unsubscribes', 'complaints', 'messages'],
     'domains': ['events', 'bounces', 'complaints', 'unsubscribes', 'messages'],
+    'mailing_lists': ['members']
 }
 KEY_PROPERTIES = {
     'domains': ['id'],
     'events': ['id'],
     'messages': ['storage_url'],
+    'mailing_lists': ['address'],
+    'members': ['address'],
     'bounces': ['domain_id', 'address', 'created_at'],
     'complaints': ['domain_id', 'address', 'created_at'],
     'unsubscribes': ['domain_id', 'address', 'created_at'],
